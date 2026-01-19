06a9a3fea4f1169f238739924e2ee9780f9fd029
h-y-r <150247768+h-y-r@users.noreply.github.com>
2026-01-19 15:52:20 +0100

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,4 +1,5 @@
 `timescale 1ns/10ps
+import transaction_class::*;
 
 module testbench;
   tri1 SCL;
@@ -27,20 +28,27 @@ module testbench;
     .SCL(SCL)
   );
 
-//   I2C_Config i2c_cfg;
-  
+   I2C_Config i2c_cfg;
+	
+
+  Transaction test_tr;
+  Transaction test_tr2;	  
   initial begin
-//     i2c_cfg = new();
-//     if (!i2c_cfg.randomize()) begin
-//       $error("blad");
-//       $finish;
-//     end
-//     dv_i2c.HIGH_PERIOD_SCL = i2c_cfg.high_period;
-//     dv_i2c.LOW_PERIOD_SCL  = i2c_cfg.low_period;
-//     dv_i2c.DATA_SETUP_TIME = i2c_cfg.setup_time;
-//     dv_i2c.RANDOM_STOP_BIT = i2c_cfg.rand_bit;
+    i2c_cfg = new();
+     if (!i2c_cfg.randomize()) begin
+       $error("blad");
+       $finish;
+     end
+     dv_i2c.HIGH_PERIOD_SCL = i2c_cfg.high_period;
+     dv_i2c.LOW_PERIOD_SCL  = i2c_cfg.low_period;
+     dv_i2c.DATA_SETUP_TIME = i2c_cfg.setup_time;
+     dv_i2c.RAND_STOP_BIT = i2c_cfg.rand_bit;
+     dv_i2c.START_SETUP_TIME = i2c_cfg.start_setup_time;
+     dv_i2c.START_HOLD_TIME = i2c_cfg.start_hold_time;
+     dv_i2c.STOP_SETUP_TIME = i2c_cfg.stop_setup_time;
+     dv_i2c.DATA_HOLD_TIME = dv_i2c.LOW_PERIOD_SCL - dv_i2c.DATA_SETUP_TIME;
     
-	$dumpfile("dump.vcd");
+    $dumpfile("dump.vcd");
     $dumpvars(0,testbench);
     #10;
     rst = 1;    
@@ -49,18 +57,21 @@ module testbench;
     #10;
     rst = 1;
     
-    //dv_i2c.writeTransaction(7'b0000111, 8'b10101010);
-	//dv_i2c.readTransaction(7'b0000111);
-    dv_i2c.writeRandomStop(7'b0000111, 8'b10101010, 2);
-	dv_i2c.genSCL();
-	dv_i2c.writeTransaction(7'b0000111, 8'b10101010);
-    //dv_i2c.genSCL();
-    //dv_i2c.sendStop();
-    //dv_i2c.genSCL();
+    fork
+    	dv_i2c.transactionDriver();
+    join_none
     
+    test_tr = new(7'b0000111, 0, ,{8'b10101010, 8'b11100011});
+    #1 dv_i2c.tr_mailbox.put(test_tr);
+    //#10 test_tr2 = new(7'b0000111, 1, 1);
+    //#1 dv_i2c.tr_mailbox.put(test_tr2);
+    //dv_i2c.writeTransaction(7'b0000111, 8'b10101010);
+    //dv_i2c.readTransaction(7'b0000111);
+    //dv_i2c.writeRandomStop(7'b0000111, 8'b10101010, 2);
+   
     $display("Simulation Finished.");
 
-    #100000
+    #1000000
     $finish(0);
   end
 
