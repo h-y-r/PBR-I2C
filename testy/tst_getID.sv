668601e827d4d3124a2164b9a129bfa0d5ac7646
PatrykKu <242165@edu.p.lodz.pl>
2026-05-24 13:35:14 +0200

    looping tests
    Updated the GET_DEVICE_ID property and added a loop for multiple transactions.

--- testy/tst_getID.sv
+++ testy/tst_getID.sv
@@ -1,7 +1,7 @@
 `define DRIVER testbench.dv_i2c
 `define TARGET testbench.tg_i2c
 `define MAIL testbench.dv_i2c.tr_mailbox
-`define RAND testbench.dv_i2c.i2c_cfg
+`define RAND testbench.i2c_cfg
 `define TRANS testbench.test_tr
 `define TARGET_BITS testbench.tg_i2c.data_send //ostatnie do zmieniania, latwiej bedzie dla roznych targetow z definicja
 
@@ -11,36 +11,38 @@ module test;
 
 property GET_DEVICE_ID;
 	@(posedge testbench.SCL)
-	(((`DRIVER.phase == M_DEVICE_ID) && (`DRIVER.bit_idx == BIT_ACK)) && (testbench.SDA == 1'b0))
-	##[1:$]
-	((`DRIVER.phase == M_SEND_ADDR_FOR_ID) && (`DRIVER.bit_idx == BIT_ACK)) |=> (testbench.SDA == 1'b0); //mozna jeszcze dodac, ale najpierw zobaczyc czy na to odpowie
+	((`DRIVER.phase == M_SEND_ADDR_FOR_ID) && (`DRIVER.bit_idx == 0)) 
+	|=> (testbench.SDA == 1'b0)//mozna jeszcze dodac, ale najpierw zobaczyc czy na to odpowie
 endproperty
 
+parameter int NUM_TRANSACTIONS = 20;
 
 initial begin
 	Transaction tr;
 
-	RAND = new();
-	if (!RAND.randomize()) begin
+	`RAND = new();
+	if (!`RAND.randomize()) begin
 	$error("blad");
 	end
 
-	DRIVER.HIGH_PERIOD_SCL = RAND.high_period;
-	DRIVER.LOW_PERIOD_SCL  = RAND.low_period;
-	DRIVER.DATA_SETUP_TIME = RAND.setup_time;
-	DRIVER.RAND_STOP_BIT = RAND.rand_bit;
-	DRIVER.START_SETUP_TIME = RAND.start_setup_time;
-	DRIVER.START_HOLD_TIME = RAND.start_hold_time;
-	DRIVER.STOP_SETUP_TIME = RAND.stop_setup_time;
-	DRIVER.DATA_HOLD_TIME = DRIVER.LOW_PERIOD_SCL - DRIVER.DATA_SETUP_TIME;	
+	`DRIVER.HIGH_PERIOD_SCL = `RAND.high_period;
+	`DRIVER.LOW_PERIOD_SCL  = `RAND.low_period;
+	`DRIVER.DATA_SETUP_TIME = `RAND.setup_time;
+	`DRIVER.RAND_STOP_BIT = `RAND.rand_bit;
+	`DRIVER.START_SETUP_TIME = `RAND.start_setup_time;
+	`DRIVER.START_HOLD_TIME = `RAND.start_hold_time;
+	`DRIVER.STOP_SETUP_TIME = `RAND.stop_setup_time;
+	`DRIVER.DATA_HOLD_TIME = `DRIVER.LOW_PERIOD_SCL - `DRIVER.DATA_SETUP_TIME;	
 	#100ns;
 		
-	getDeviceID(7'b0000111);
-	
-	#25us;
+	for (int i = 0; i < NUM_TRANSACTIONS; i++) begin		
+		`DRIVER.getDeviceID(7'b0000111);	
+		#100ns;
+	end
+	$finish(0);
 end
 
 tst_getDeviceID: assert property (GET_DEVICE_ID) $display("chk_getDeviceID PASSED!");
 		else $error("chk_getDeviceID FAILED!");	
 		
-end module
+endmodule
